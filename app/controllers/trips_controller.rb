0f9b5a8edbc4bc5d3dc548c7d12d5c65b9f7c7b2
kpomalle11 <kevin.p.omalley11@gmail.com>
2024-08-13 05:19:37 +0000

    altered the systems prompt, added clear inputs, added a edit trip page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,5 @@
 class TripsController < ApplicationController
-  require 'openai'
+  require "openai"
 
   def index
     matching_trips = Trip.all
@@ -53,11 +53,18 @@ class TripsController < ApplicationController
     message_list = [
       {
         "role" => "system",
-        "content" => "You are a helpful travel assistant helping the users plan a trip according to their message.  It would be help to suggest activities to do, places to stay, restaurants to go to, and other cool things based on the specific location or surrounding area.  Essentially, build out a trip itinerary for the user based on the message context.",
+        "content" => "You are a helpful travel assistant helping users plan a trip. Your task is to generate a complete travel itinerary in the form of a JSON object. Only return the JSON object, without any additional text or explanations. The JSON object should include these fields: arrival_date, departure_date, title, activity_id, and description. Please return the response in a JSON format like this:
+        {
+          \"arrival_date\": \"<arrival_date>\",
+          \"departure_date\": \"<departure_date>\",
+          \"title\": \"<title>\",
+          \"activity_id\": \"<activity_id>\",
+          \"description\": \"<description>\"
+        }.",
       },
       {
         "role" => "user",
-        "content" => "I am planning a trip with the following details: #{trip_params}. Please provide suggestions and advice.",
+        "content" => "I am planning a trip with the following details: #{trip_params}. Please provide suggestions and advice, and return the data in JSON format only.",
       },
     ]
 
@@ -65,27 +72,48 @@ class TripsController < ApplicationController
       parameters: {
         model: "gpt-4",
         messages: message_list,
-        max_tokens: 150,
+        max_tokens: 500,
       },
     )
 
-    @chatgpt_response = response.dig("choices", 0, "message", "content")
+    response_content = response.dig("choices", 0, "message", "content").strip
 
-    # You can either render this on a new page or show it in the current view.
-    render plain: @chatgpt_response
+    # Attempt to parse the response
+    begin
+      parsed_response = JSON.parse(response_content)
+    rescue JSON::ParserError => e
+      # Handle parsing error (for example, log it and inform the user)
+      redirect_to(new_trip_path, alert: "There was an error processing your request. Please try again.")
+      return
+    end
+
+    # Create a new trip using the ChatGPT response
+    the_trip = Trip.create!(
+      owner_id: trip_params[:owner_id],
+      description: parsed_response["description"],
+      arrival_date: parsed_response["arrival_date"],
+      departure_date: parsed_response["departure_date"],
+      title: parsed_response["title"],
+      activity_id: parsed_response["activity_id"],
+      published: false,  # Set to false by default, the user can publish later
+    )
+
+    # Redirect to the edit page for the newly created trip
+    redirect_to("/trips/#{the_trip.id}/edit", notice: "Trip created based on ChatGPT suggestions. Please review and update.")
   end
 
   def update
-    the_id = params.fetch("path_id")
-    the_trip = Trip.where({ :id => the_id }).at(0)
+    the_trip = Trip.find(params[:path_id])
 
-    the_trip.owner_id = params.fetch("query_owner_id")
-    the_trip.description = params.fetch("query_description")
-    the_trip.arrival_date = params.fetch("query_arrival_date")
-    the_trip.departure_date = params.fetch("query_departure_date")
-    the_trip.title = params.fetch("query_title")
-    the_trip.activity_id = params.fetch("query_activity_id")
-    the_trip.published = params.fetch("query_published", false)
+    the_trip.update(
+      owner_id: params[:trip][:owner_id],
+      description: params[:trip][:description],
+      arrival_date: params[:trip][:arrival_date],
+      departure_date: params[:trip][:departure_date],
+      title: params[:trip][:title],
+      activity_id: params[:trip][:activity_id],
+      published: params[:trip][:published],
+    )
 
     if the_trip.valid?
       the_trip.save
@@ -93,6 +121,15 @@ class TripsController < ApplicationController
     else
       redirect_to("/trips/#{the_trip.id}", { :alert => the_trip.errors.full_messages.to_sentence })
     end
+  end
+
+  def edit
+    @trip = Trip.find(params[:path_id])
+  end
+
+  # Redirect back to the form with empty parameters
+  def clear_form
+    redirect_to new_trip_path
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,27 @@
-
 Rails.application.routes.draw do
   devise_for :users
 
-  post '/insert_trip', to: 'trips#create'
-  post '/ask_chatgpt', to: 'trips#ask_chatgpt'
+  post "/insert_trip", to: "trips#create"
+  post "/ask_chatgpt", to: "trips#ask_chatgpt"
 
   #homepage
 
   root to: "trips#index"
-  
+
   # Routes for the Activity resource:
 
   # CREATE
   post("/insert_activity", { :controller => "activities", :action => "create" })
-          
+
   # READ
   get("/activities", { :controller => "activities", :action => "index" })
-  
+
   get("/activities/:path_id", { :controller => "activities", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_activity/:path_id", { :controller => "activities", :action => "update" })
-  
+
   # DELETE
   get("/delete_activity/:path_id", { :controller => "activities", :action => "destroy" })
 
@@ -32,16 +31,16 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_comment", { :controller => "comments", :action => "create" })
-          
+
   # READ
   get("/comments", { :controller => "comments", :action => "index" })
-  
+
   get("/comments/:path_id", { :controller => "comments", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_comment/:path_id", { :controller => "comments", :action => "update" })
-  
+
   # DELETE
   get("/delete_comment/:path_id", { :controller => "comments", :action => "destroy" })
 
@@ -51,20 +50,23 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_trip", { :controller => "trips", :action => "create" })
-          
+
   # READ
   get("/trips", { :controller => "trips", :action => "index" })
-  
+
   get("/trips/:path_id", { :controller => "trips", :action => "show" })
-  
+
   # UPDATE
-  
-  post("/modify_trip/:path_id", { :controller => "trips", :action => "update" })
-  
+  post("/modify_trip/:path_id", to: "trips#update", as: "modify_trip")
+  get("/trips/:path_id/edit", to: "trips#edit", as: "edit_trip")
+  get("/clear_trip_form", { :controller => "trips", :action => "clear_form" })
+
+  # post("/modify_trip/:path_id", { :controller => "trips", :action => "update" })
+  # get("/trips/:path_id/edit", { :controller => "trips", :action => "edit" })
+
   # DELETE
   get("/delete_trip/:path_id", { :controller => "trips", :action => "destroy" })
 
   #------------------------------
 
-  
 end
